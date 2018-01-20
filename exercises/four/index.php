94a5307c236d6fb008d404ba0dfb1ff3eaac9e5a
MerrySean <zilex.seryth@gmail.com>
2018-01-21 05:21:29 +0800

    Get rid of Practical list

--- exercises/four/index.php
+++ exercises/four/index.php
@@ -8,7 +8,6 @@
           <li><a href="./Logout">Logout</a></li>
           <li><a href="./User/Update">Update Profile</a></li>
           <li><a href="./List/Projects">List of All Projects</a></li>
-          <li><a href="./List/Practical">List of All Small-Usefull-Projects (Practical)</a></li>
           <li><a href="./Add/Projects">Add Projects</a></li>
         </ul>
     </body>
